64fa1afd79e0faed3797842ada1ad4f251a48bce
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-06 12:07:47 -0600

    added assignment

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            //addition
+            Console.WriteLine("Enter two numbers to sum");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x + y);
+            
+
+            //subtraction
+            Console.WriteLine("Enter two numbers to find the difference");
+            int f = Convert.ToInt32(Console.ReadLine());
+            int g = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(f - g);
+            
+
+            //multiplication
+            Console.WriteLine("Enter two numbers to multiply");
+            int q = Convert.ToInt32(Console.ReadLine());
+            int w = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(q * w);
+            
+
+            //division
+            Console.WriteLine("Enter two numbers to divide");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int quotient = (a % b);
+            Console.WriteLine($"{a}/{b} is {a/b} remainder {quotient}.");
+            
+
+
+            //Exercise 2
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}" );
+            
+
+            //Thought Exercise
+            var i = 3; // i is assigned the value of 3
+            var j = 4; // j is assigned the value of 4
+            var k = ++i * j++; // k is assigned the value of i + j. i s given an increment of 1 prior to the code being stated, making i = 4. j is given an increment of 1 only after the
+                               // j is restated in the code, however since it never was, j is still j = 4. making k = 4 * 4. k = 16. ...i hope
+
+            Console.WriteLine(k); // ouputs 16? YAY
+
+
+
         }
     }
 }
