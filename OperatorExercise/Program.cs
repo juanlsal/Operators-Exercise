233c69cac4f190b49a33994b6169ff354e78e7bf
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-03-06 17:26:28 -0600

    added version with notes

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,30 +6,44 @@
         {
             
             //addition
-            Console.WriteLine("Enter two numbers to sum");
-            int x = Convert.ToInt32(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter two numbers to sum");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //or
+            int x = 5;
+            int y = 10;
             Console.WriteLine(x + y);
-            
+            //output 15
 
             //subtraction
-            Console.WriteLine("Enter two numbers to find the difference");
-            int f = Convert.ToInt32(Console.ReadLine());
-            int g = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter two numbers to find the difference");
+            //int f = Convert.ToInt32(Console.ReadLine());
+            //int g = Convert.ToInt32(Console.ReadLine());
+            //or
+            int f = 100;
+            int g = 25;
             Console.WriteLine(f - g);
-            
+            //output 75
+
 
             //multiplication
-            Console.WriteLine("Enter two numbers to multiply");
-            int q = Convert.ToInt32(Console.ReadLine());
-            int w = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter two numbers to multiply");
+            //int q = Convert.ToInt32(Console.ReadLine());
+            //int w = Convert.ToInt32(Console.ReadLine());
+            //or
+            int q = 10;
+            int w = 2;
             Console.WriteLine(q * w);
-            
+            //output 20
+
 
             //division
-            Console.WriteLine("Enter two numbers to divide");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter two numbers to divide");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int quotient = (a % b);
+            int a = 17;
+            int b = 4;
             int quotient = (a % b);
             Console.WriteLine($"{a}/{b} is {a/b} remainder {quotient}.");
             
